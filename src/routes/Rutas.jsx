0a79cd6fad9f28e42abed46a04d89c47fe5b2af5
el3azar <otoeleazarivas@gmail.com>
2025-06-23 01:42:01 -0600

    Implementacion de login mediante token y creacion de rutas para modulos

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainLayout from '../components/MainLayout';
+import Login from '../components/Login'
 import DashBoardSales from '../components/sales/DashBoardSales'
 
 export default function Rutas() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
+      <Routes>
+        {/* Ruta pública, sin layout */}
+        <Route path='/' element={<Login />} />
+        
+        {/* Ruta fuera del sidebar para el administrador */}
+        <Route path="/admin" element={<div className='text-dark'>Administración</div>} />
+        {/* Agrupa rutas protegidas dentro del layout */}
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<div  className='text-dark'>DashBoard General de los modulos para asistente</div>} />
           <Route path="/ventas" element={<DashBoardSales />} />
           <Route path="/cuentas" element={<div>Cuentas por Cobrar</div>} />
-          <Route path="/admin" element={<div>Administración</div>} />
+          
           <Route path="/inventario" element={<div>Inventario</div>} />
-          {/* Ruta por defecto */}
+          {/* Ruta por defecto para cualquier otra */}
           <Route path="*" element={<DashBoardSales />} />
-        </Routes>
-      </MainLayout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }   
